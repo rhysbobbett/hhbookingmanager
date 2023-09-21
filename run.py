33053d22abbd1548bb19fb9dc5c671dadc383660
Rhys Bobbett <120944779+rhysbobbett@users.noreply.github.com>
2023-09-21 18:50:43 +0100

    Edit run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import os
-from hhbookingmanager import app
+from hhbookingmanager.app import app
 
 if __name__ == "__main__":
-    app.run(
+    app(
         host=os.environ.get("IP", ""),
         port=int(os.environ.get("PORT", "")),
         debug=False
