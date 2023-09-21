8db7ea7b442f3cfb9197408776aba62f823cd01d
Rhys Bobbett <120944779+rhysbobbett@users.noreply.github.com>
2023-09-21 13:17:24 +0100

    Add environ.get routing

--- app.py
+++ app.py
@@ -486,3 +486,9 @@ def internal_server_error(e):
     return render_template('error505.html'), 500
 
 
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
